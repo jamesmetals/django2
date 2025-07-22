b2482973eb9fdb6e1b29d8354dfdc6ce89fbaee9
jamesmetals <jamesricb@gmail.com>
2025-07-22 09:13:18 -0300

    Remover desktop.ini do reposit├│rio

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path("tarefas/",include("home.urls"),
     path("tarefas/",include("tarefas.urls")),
 ]
--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    concluida = models.BooleanField(default=False)
+    criado_em = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo

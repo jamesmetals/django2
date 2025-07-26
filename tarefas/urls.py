eb511630ca5f8f04362bab0283468dc62f7987f4
jamesmetals <jamesricb@gmail.com>
2025-07-26 12:43:16 -0300

    tudo certo

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.home, name= "home"),
-    path("adicionar", views.add, name= "adicionar"),
+    path("adicionar", views.adicionar, name= "adicionar"),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import TarefaForm
 
 frase=("Adiciionar")
 
 def home(request):
     return render(request, 'tarefas/home.html')
 
-def add(request):
-    # Logic for adding a task would go here
-    return HttpResponse (frase)
-
 def adicionar(request):
-    return render(request, 'adicionar/home.html')
+    form = TarefaForm()
+    return render(request, 'tarefas/adicionar.html', {'form': form})
+
+#def adicionar(request):
+ # return render(request, 'adicionar/home.html')

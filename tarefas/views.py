053aad64e8de83df3ac790967e7d48cd10a1a032
jamesmetals <jamesricb@gmail.com>
2025-07-29 18:22:35 -0300

    adicionado salvar no formulario

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .forms import TarefaForm
-
+from django.shortcuts import redirect
 frase=("Adiciionar")
 
 def home(request):
     return render(request, 'tarefas/home.html')
 
 def adicionar(request):
-    form = TarefaForm()
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("Tarefa adicionada com sucesso!")
+            return redirect('home')
+    else:
+        form = TarefaForm()
     return render(request, 'tarefas/adicionar.html', {'form': form})
